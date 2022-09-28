df3a7832f62a4216055589f205e866c8fb26a99d
brightdevs <38365682+e-rojas@users.noreply.github.com>
2022-09-28 19:37:56 -0300

    basic type definitions

--- src/index.js
+++ src/index.js
@@ -2,17 +2,67 @@ import { GraphQLServer } from 'graphql-yoga';
 
 // Type definitions (schema) application schema
 const typeDefs = `type Query {
-    hello: String!
-}`;
+    me: User!
+    post: Post!
+}
 
-// Resolvers
+type User {
+    id: ID!
+    name: String!
+    email: String!
+    age: Int
+}
+
+type Post {
+    id: ID!
+    title: String!
+    body: String!
+    published: Boolean!
+}
+
+`;
+
 const resolvers = {
   Query: {
-    hello() {
-      return 'This is my first query!';
+    me() {
+      return {
+        id: '123098',
+        name: 'Mike',
+        email: 'mike@gmail.com',
+        age: 28,
+      };
+    },
+    post() {
+      return {
+        id: '123098',
+        title: 'GraphQL 101',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        published: false,
+      };
     },
   },
 };
+
+// Resolvers
+// const resolvers = {
+//   Query: {
+//    id() {
+//     return 'abc123';
+//    },
+//     name() {
+//         return 'Jerry'
+//     },
+//     age() {
+//         return 27
+//     },
+//     employed() {
+//         true
+//     },
+//     gpa(){
+//         return 3.23
+//     }
+//   },
+// };
 
 // Create the GraphQL Yoga Server
 const server = new GraphQLServer({
